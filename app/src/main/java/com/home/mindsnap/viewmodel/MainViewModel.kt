ef3149ec8ed05c17cbc4578e7c961c21d96db0c6
choi-hyeseong <bd2845@kakao.com>
2023-11-17 15:26:47 +0900

    mainviewmodel add some logic (isFirstJoin)

--- app/src/main/java/com/home/mindsnap/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/home/mindsnap/viewmodel/MainViewModel.kt
@@ -1,6 +1,16 @@
 package com.home.mindsnap.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.home.mindsnap.usecase.GetUserFirstJoined
 
-class MainViewModel : ViewModel() {
+class MainViewModel(private val getUserFirstJoined: GetUserFirstJoined) : ViewModel() {
+
+    fun isFirstJoined() : LiveData<Boolean> {
+        val liveData = MutableLiveData<Boolean>()
+        liveData.value = getUserFirstJoined.isFirstJoined()
+        return liveData
+    }
+
 }
