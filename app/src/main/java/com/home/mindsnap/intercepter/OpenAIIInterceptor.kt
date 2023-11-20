e78255e7c3046763ef0571137b84e09948a7793b
choi-hyeseong <bd2845@kakao.com>
2023-11-20 12:35:27 +0900

    remove unused param

--- app/src/main/java/com/home/mindsnap/intercepter/OpenAIIInterceptor.kt
+++ app/src/main/java/com/home/mindsnap/intercepter/OpenAIIInterceptor.kt
@@ -3,7 +3,7 @@ package com.home.mindsnap.intercepter
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class OpenAIIInterceptor(private val url: String, private val token: String) : Interceptor {
+class OpenAIIInterceptor(private val token: String) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request =
             chain.request().newBuilder().addHeader("Content-Type", "application/json")
