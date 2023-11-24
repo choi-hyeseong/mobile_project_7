9dbc7d52c5c0cf0eac65a4befcf878c5dab15549
choi-hyeseong <bd2845@kakao.com>
2023-11-24 21:36:33 +0900

    refactor repository name

--- app/src/main/java/com/home/mindsnap/repository/image/OpenAIImageGenRepository.kt
+++ app/src/main/java/com/home/mindsnap/repository/image/OpenAIImageGenRepository.kt
@@ -1,12 +1,11 @@
 package com.home.mindsnap.repository.image
 
-import android.content.Context
 import android.graphics.Bitmap
 import com.home.mindsnap.repository.image.dao.ImageGenDao
 import com.home.mindsnap.type.ArtStyle
 
 
-class LocalImageGenRepository(private val genDao: ImageGenDao) :
+class OpenAIImageGenRepository(private val genDao: ImageGenDao) :
     ImageGenRepository {
     //로컬에 저장할려면 application context 필요
     override suspend fun generateImage(prompt: String, style: ArtStyle): Bitmap {
