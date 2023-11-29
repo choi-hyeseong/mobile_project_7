42d5f8f82e0451c8467c6f108a7e8fc5f30a4ebb
choi-hyeseong <bd2845@kakao.com>
2023-11-29 18:22:33 +0900

    add share image usecase

--- app/src/main/java/com/home/mindsnap/viewmodel/GalleryViewModel.kt
+++ app/src/main/java/com/home/mindsnap/viewmodel/GalleryViewModel.kt
@@ -1,17 +1,20 @@
 package com.home.mindsnap.viewmodel
 
+import android.content.Intent
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.home.mindsnap.model.Image
 import com.home.mindsnap.usecase.GetAllImages
+import com.home.mindsnap.usecase.ShareImage
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class GalleryViewModel(private val getAllImages: GetAllImages) : ViewModel() {
+class GalleryViewModel(private val getAllImages: GetAllImages, private val shareImage: ShareImage) : ViewModel() {
 
     val imageLiveData : MutableLiveData<List<Image>> = MutableLiveData()
+    val intentLiveData : MutableLiveData<Intent> = MutableLiveData()
 
     init {
         getAllImage()
@@ -21,5 +24,9 @@ class GalleryViewModel(private val getAllImages: GetAllImages) : ViewModel() {
         CoroutineScope(Dispatchers.IO).launch {
             imageLiveData.postValue(getAllImages.getAllImages())
         }
+    }
+
+    fun shareImage(fileName : String) {
+        shareImage.shareImage(fileName)
     }
 }
