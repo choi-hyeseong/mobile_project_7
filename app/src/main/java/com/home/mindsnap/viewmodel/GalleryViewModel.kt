28324c53cba9fc2f21e5dcbb275c89ea9799ca09
choi-hyeseong <bd2845@kakao.com>
2023-11-29 19:46:18 +0900

    livedata apply..

--- app/src/main/java/com/home/mindsnap/viewmodel/GalleryViewModel.kt
+++ app/src/main/java/com/home/mindsnap/viewmodel/GalleryViewModel.kt
@@ -1,9 +1,11 @@
 package com.home.mindsnap.viewmodel
 
 import android.content.Intent
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.home.mindsnap.LOG_HEADER
 import com.home.mindsnap.model.Image
 import com.home.mindsnap.usecase.GetAllImages
 import com.home.mindsnap.usecase.ShareImage
@@ -27,6 +29,7 @@ class GalleryViewModel(private val getAllImages: GetAllImages, private val share
     }
 
     fun shareImage(fileName : String) {
-        shareImage.shareImage(fileName)
+        // 맞다 livedata set..
+        intentLiveData.value = shareImage.shareImage(fileName)
     }
 }
