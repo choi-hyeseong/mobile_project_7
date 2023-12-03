44f9d8f5807fc16d7056ac516a4fb99575bf23f5
choi-hyeseong <bd2845@kakao.com>
2023-12-03 22:49:21 +0900

    add button visibility when screen rotate

--- app/src/main/java/com/home/mindsnap/fragment/tutorial/CoachMarkFragment.kt
+++ app/src/main/java/com/home/mindsnap/fragment/tutorial/CoachMarkFragment.kt
@@ -18,8 +18,8 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class CoachMarkFragment : Fragment() {
 
-    private val viewModel : CoachMarkViewModel by viewModels()
-    private var callback : ActivityCallback? = null
+    private val viewModel: CoachMarkViewModel by viewModels()
+    private var callback: ActivityCallback? = null
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -36,18 +36,19 @@ class CoachMarkFragment : Fragment() {
         }
 
         viewModel.coachLiveData.observe(viewLifecycleOwner) {
-            val drawable : Drawable? = when (it) {
+            val drawable: Drawable? = when (it) {
                 1 -> ContextCompat.getDrawable(requireContext(), R.drawable.coach_2)
                 2 -> ContextCompat.getDrawable(requireContext(), R.drawable.coach_3)
                 3 -> ContextCompat.getDrawable(requireContext(), R.drawable.coach_4)
                 4 -> ContextCompat.getDrawable(requireContext(), R.drawable.coach_5)
                 5 -> ContextCompat.getDrawable(requireContext(), R.drawable.coach_6)
                 6 -> ContextCompat.getDrawable(requireContext(), R.drawable.coach_7)
-                7 -> {
-                    bind.btnStart.visibility = View.VISIBLE
+                else -> {
+                    //화면 회전시 뷰 안뜨는 문제 해결
+                    if (it != 0)
+                        bind.btnStart.visibility = View.VISIBLE
                     null
                 }
-                else -> null
             }
             drawable?.let { bind.coachImage.setImageDrawable(it) }
         }
